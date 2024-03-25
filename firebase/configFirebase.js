267707cad05fe5183bd5f80add421e745dde3739
cristianCor19 <cordobac96@gmail.com>
2024-03-24 21:09:01 -0500

    test

--- firebase/configFirebase.js
+++ firebase/configFirebase.js
@@ -2,18 +2,26 @@ import fs from 'fs';
 import path from 'path';
 import { PROJECT_ID, PRIVATE_KEY_ID, PRIVATE_KEY, CLIENT_EMAIL, CLIENT_ID } from '../config.js';
 
-// const filePath = new URL('./firabase.json', import.meta.url).pathname.substring(1);
-const filePath = path.resolve(__dirname, './firabase.json');
+
+
+const filePath = new URL('firabese.json', import.meta.url).pathname.substring(1);
+// const filePath = path.join(__dirname, './firabase.json');
+// import filePath from "./firabase.json" assert { type: "json" };
+
 
 console.log(filePath);
 
 let jsonData = fs.readFileSync(filePath, 'utf-8');
+
+// console.log(jsonData);
 
 jsonData = jsonData.replace(/PROJECT_ID/g, PROJECT_ID)
                    .replace(/PRIVATE_KEY_ID/g, PRIVATE_KEY_ID)
                    .replace(/PRIVATE_KEY/g, PRIVATE_KEY)
                    .replace(/CLIENT_EMAIL/g, CLIENT_EMAIL)
                    .replace(/CLIENT_ID/g, CLIENT_ID)
+
+
 
 export const configAdminFirebase = JSON.parse(jsonData);
 
