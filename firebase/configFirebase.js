a3ef3389931e80652326b0947d1a6dbed55184fa
cristianCor19 <cordobac96@gmail.com>
2024-03-24 23:14:26 -0500

    update method configFirebase

--- firebase/configFirebase.js
+++ firebase/configFirebase.js
@@ -4,24 +4,36 @@ import { PROJECT_ID, PRIVATE_KEY_ID, PRIVATE_KEY, CLIENT_EMAIL, CLIENT_ID } from
 
 
 
-const filePath = new URL('firabese.json', import.meta.url).pathname.substring(1);
-// const filePath = path.join(__dirname, './firabase.json');
-// import filePath from "./firabase.json" assert { type: "json" };
+// const filePath = new URL('./firabese.json', import.meta.url).pathname.substring(1);
+
+// console.log(filePath);
+
+// let jsonData = fs.readFileSync(filePath, 'utf-8');
+
+let jsonData = {
+    "type": "service_account",
+    "project_id": "PROJECT_ID",
+    "private_key_id": "PRIVATE_KEY_ID",
+    "private_key": "PRIVATE_KEY",
+    "client_email": "CLIENT_EMAIL",
+    "client_id": "CLIENT_ID",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapcom/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.crobot/v1/metadata/x5firebase-adminsdk-g4ebm%40test-firebase-a4d1d.igserviceaccount.com",
+    "universe_domain": "googleapis.com"
+}
 
 
-console.log(filePath);
+            
+let jsonString = JSON.stringify(jsonData);
 
-let jsonData = fs.readFileSync(filePath, 'utf-8');
 
-// console.log(jsonData);
-
-jsonData = jsonData.replace(/PROJECT_ID/g, PROJECT_ID)
+jsonData = jsonString.replace(/PROJECT_ID/g, PROJECT_ID)
                    .replace(/PRIVATE_KEY_ID/g, PRIVATE_KEY_ID)
                    .replace(/PRIVATE_KEY/g, PRIVATE_KEY)
                    .replace(/CLIENT_EMAIL/g, CLIENT_EMAIL)
                    .replace(/CLIENT_ID/g, CLIENT_ID)
-
-
 
 export const configAdminFirebase = JSON.parse(jsonData);
 
